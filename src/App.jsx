ad8c7532c8a82834883c85f172717275b6511207
Sara Salzano <sarasalzano95@gmail.com>
2026-01-05 10:36:41 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import useSwitch from "./components/useSwitch";
+
+function App() {
+  const [isOn, toggle] = useSwitch();
+  return (
+    <div>
+      <h1>Il valore è: {isOn ? "ON" : "OFF"}</h1>
+      <button onClick={toggle}>Cambia Stato</button>
+    </div>
+  );
+}
+
+export default App;
